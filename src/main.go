3255cfce537c6689f5d26ddce4a8d546985e4339
fabien <fabien.thetis@gmail.com>
2016-05-03 18:37:32 +0200

    commit the main file of the project

---
+++ src/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	//"os"
+	/*"fmt"
+	"log"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"*/
+
+//"net/http"
+	"./package1"
+	"./package_redis"
+	"./package_gin"
+	"./package_mongodb"
+	"./package_elast"
+//   "./package_mysql"
+)
+
+
+
+func main() {
+
+
+
+	package_mongodb.InsertMongo()
+
+	package1.Test();
+
+	package1.ReadJson2();
+
+	package_redis.Read("db to read")
+
+	package_elast.Read()
+
+	package_gin.StartRouter()
+	// package_mysql.Read()
+
+
+
+}
+
